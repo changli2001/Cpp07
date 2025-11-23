3e1baefc87514089d31c43df4fcf34d2d3494f29
Othmane Changli <ochangli@e3r1p9.1337.ma>
2025-11-23 10:57:24 +0100

    Finish ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+    #include <iostream>
+
+    template <typename T>
+    void    swap(T &a, T &b)
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    template <typename T>
+    T   min(const T& a, const T& b)
+    {
+        return (b <= a) ? b : a;
+    }
+
+    template <typename T>
+    T   max(const T& a, const T& b)
+    {
+        T max;
+        (b >= a) ? max = b : max = a;
+        return max;
+    }
